942b8e73b7359b87c900e30056cf0a43410ecc0b
ksuljh <a2026964038@gmail.com>
2026-05-15 07:32:23 +0900

    Add files via upload

---
+++ 0508/Lab/Lab4.py
@@ -0,0 +1,6 @@
+import random
+s = "0123456789"
+passlen = 4
+
+p = "".join(random.sample(s, passlen ))
+print(p)
---
+++ 0508/Other/SetC.py
@@ -0,0 +1,12 @@
+fruits = { "apple", "banana", "grape" }
+if "apple" in fruits:
+    print("집합 안에 apple이 있습니다.")
+
+fruits ={"apple","banana","grape"}
+for x in fruits:
+    print(x, end=" ")
+
+fruits ={"apple","banana","grape"}
+for x in sorted(fruits):
+    print(x, end=" ")
+
